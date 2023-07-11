9f8c3f7cfd8a302a6d1cc7ef03dec513ebad59dc
X474X <paultudorstefan@gmail.com>
2023-07-11 13:22:25 +0300

    Finished static site

---
+++ script.js
@@ -0,0 +1,4 @@
+const ecran = document.querySelector("screen");
+const numeros = document.querySelector("numero");
+const resultat = document.querySelector("egalite");
+const calculator = document.querySelector("casio");
