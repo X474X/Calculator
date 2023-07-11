36bbb58dbce3d8fb1e322f5a90e36d6758678654
X474X <paultudorstefan@gmail.com>
2023-07-11 14:24:40 +0300

    Finished dinamic site

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const ecran = document.querySelector("screen");
-const numeros = document.querySelector("numero");
-const resultat = document.querySelector("egalite");
-const calculator = document.querySelector("casio");
+const ecuation = document.querySelector("span");
+const buttons = document.querySelectorAll(".btn");
+const fraternite = document.querySelector(".egalite");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    if (buttons[i].innerHTML === "C") {
+      ecuation.innerHTML = "";
+    } else {
+      ecuation.innerHTML = ecuation.innerHTML + buttons[i].innerHTML;
+    }
+  });
+}
+
+fraternite.addEventListener("click", () => {
+  if (ecuation.innerHTML !== "") {
+    if (parseInt(ecuation.innerHTML[ecuation.innerHTML.length - 1])) {
+      ecuation.innerHTML = eval(ecuation.innerHTML);
+    } else {
+      ecuation.innerHTML = "Error";
+    }
+  }
+});
